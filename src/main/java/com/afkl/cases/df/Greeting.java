18bf80218cda50a0f6acad2b02647ea1c4b7beb4
pvaughan <pvaughan@mobiquityinc.com>
2016-12-02 22:15:32 +0100

    Initial commit

---
+++ src/main/java/com/afkl/cases/df/Greeting.java
@@ -0,0 +1,23 @@
+package com.afkl.cases.df;
+
+/**
+ * Created by pvaughan on 02/12/2016.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
